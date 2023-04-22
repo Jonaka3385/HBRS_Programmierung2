652f12f8b9d9d7c4f9e4838886f5e29efe48c04b
Jonaka <jonakactfs@gmail.com>
2023-04-22 15:40:51 +0200

    (Josephus Exception Fehler behoben)

--- src/mainClasses/Josephus.java
+++ src/mainClasses/Josephus.java
@@ -4,8 +4,8 @@ import speicherKlassen.RDVL;
 
 public class Josephus {
     public int loese(int n, int k) throws IllegalStateException {
-        if (n < 0) throw new IllegalStateException("n muss größer 0 sein");
-        if (k < 0) throw new IllegalStateException("k muss größer 0 sein");
+        if (n <= 0) throw new IllegalStateException("n muss größer 0 sein");
+        if (k <= 0) throw new IllegalStateException("k muss größer 0 sein");
         RDVL<Integer> r = new RDVL<>();
         for (int i = 1; i <= n; i++) {
             r.add(i);
