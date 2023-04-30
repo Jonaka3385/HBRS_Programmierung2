4e7a2aa5f44c2c45d85dbde78b98e807267d5b88
Jonaka <jonakactfs@gmail.com>
2023-05-01 00:50:55 +0200

    Überarbeitung der Strukturen

---
+++ src/main_classes/Josephus.java
@@ -0,0 +1,19 @@
+package main_classes;
+
+import speicher.RDVL;
+
+public class Josephus {
+    public int loese(int n, int k) throws IllegalStateException {
+        if (n <= 0) throw new IllegalStateException("n muss größer 0 sein");
+        if (k <= 0) throw new IllegalStateException("k muss größer 0 sein");
+        RDVL<Integer> r = new RDVL<>();
+        for (int i = 1; i <= n; i++) {
+            r.add(i);
+        }
+        for (int i = 1; i < n; i++) {
+            r.next(k-1);
+            r.remove();
+        }
+        return r.element();
+    }
+}
