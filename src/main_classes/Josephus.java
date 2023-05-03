08860bd61e559bccd1cd08cae5cce1490fa16ea7
Jonaka <jonakactfs@gmail.com>
2023-05-03 21:53:44 +0200

    kleine Überarbeitungen und weitere Tests

--- src/main_classes/Josephus.java
+++ src/main_classes/Josephus.java
@@ -4,8 +4,7 @@ import speicher.RDVL;
 
 public class Josephus {
     public int loese(int n, int k) throws IllegalStateException {
-        if (n <= 0) throw new IllegalStateException("n muss größer 0 sein");
-        if (k <= 0) throw new IllegalStateException("k muss größer 0 sein");
+        if (n <= 0 || k <= 0) throw new IllegalStateException("n muss größer 0 sein");
         RDVL<Integer> r = new RDVL<>();
         for (int i = 1; i <= n; i++) {
             r.add(i);
