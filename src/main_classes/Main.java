d0d5782d3bdfa159c4a16d320952c420b645f62b
Jonaka <jonakactfs@gmail.com>
2023-05-14 19:01:59 +0200

    MyFloat Class

--- src/main_classes/Main.java
+++ src/main_classes/Main.java
@@ -1,9 +1,21 @@
 package main_classes;
 
-import projekt_euler.ID002EvenFibonacciNumbers;
+import speicher.MyFloat;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ID002EvenFibonacciNumbers.method(1, 2, 4000000));
+        MyFloat m1 = new MyFloat(1, 50, 2);
+        MyFloat m2 = new MyFloat(2, 50, 2);
+        m1.add(m2);
+        System.out.println(m1);
+        m1 = new MyFloat(1, 50, 2);
+        m1.sub(m2);
+        System.out.println(m1);
+        m1 = new MyFloat(1, 50, 2);
+        m1.mul(m2);
+        System.out.println(m1);
+        m1 = new MyFloat(1, 50, 2);
+        m1.div(m2);
+        System.out.println(m1);
     }
 }
--- src/speicher/MyFloat.java
+++ src/speicher/MyFloat.java
@@ -1,4 +1,127 @@
 package speicher;
 
+import org.jetbrains.annotations.NotNull;
+
+import static java.lang.Math.pow;
+
+
 public class MyFloat {
+    private int vorkomma;
+    private int nachkomma;
+    private final int potenziert;
+
+    public MyFloat(int vorkomma, int nachkomma, int stellen) {
+        this.vorkomma = vorkomma;
+        this.nachkomma = nachkomma;
+        this.potenziert = (int) pow(10, stellen);
+    }
+
+    public int vorkomma() {
+        return vorkomma;
+    }
+
+    public int nachkomma() {
+        return nachkomma;
+    }
+
+    public int potenziert() {
+        return potenziert;
+    }
+
+    public void neg(){
+        vorkomma *= -1;
+        nachkomma *= -1;
+    }
+
+    public void add(@NotNull MyFloat myFloat) {
+        int myVorkomma = myFloat.vorkomma();
+        int myNachkomma = myFloat.nachkomma();
+        int myPotenziert = myFloat.potenziert();
+
+        if (potenziert < myPotenziert) myNachkomma /= (double) myPotenziert / potenziert;
+        else if (potenziert > myPotenziert) myNachkomma *= (double) potenziert / myPotenziert;
+
+        vorkomma = vorkomma + myVorkomma;
+        nachkomma = nachkomma + myNachkomma;
+        while (nachkomma >= potenziert) {
+            vorkomma++;
+            nachkomma -= potenziert;
+        }
+        while (nachkomma < 0) {
+            vorkomma--;
+            nachkomma += potenziert;
+        }
+    }
+
+    public void sub(@NotNull MyFloat myFloat) {
+        int myVorkomma = myFloat.vorkomma();
+        int myNachkomma = myFloat.nachkomma();
+        int myPotenziert = myFloat.potenziert();
+
+        if (potenziert < myPotenziert) myNachkomma /= (double) myPotenziert / potenziert;
+        else if (potenziert > myPotenziert) myNachkomma *= (double) potenziert / myPotenziert;
+
+        vorkomma = vorkomma - myVorkomma;
+        nachkomma = nachkomma - myNachkomma;
+        while (nachkomma >= potenziert) {
+            vorkomma++;
+            nachkomma -= potenziert;
+        }
+        while (nachkomma < 0) {
+            vorkomma--;
+            nachkomma += potenziert;
+        }
+    }
+
+    public void mul(@NotNull MyFloat myFloat) {
+        int myVorkomma = myFloat.vorkomma();
+        int myNachkomma = myFloat.nachkomma();
+        int myPotenziert = myFloat.potenziert();
+
+        if (potenziert < myPotenziert) myNachkomma /= (double) myPotenziert / potenziert;
+        else if (potenziert > myPotenziert) myNachkomma *= (double) potenziert / myPotenziert;
+
+        int tmp = (((vorkomma * potenziert) + nachkomma) * ((myVorkomma * potenziert) + myNachkomma));
+
+        vorkomma = 0;
+        nachkomma = tmp / potenziert;
+        while (nachkomma >= potenziert) {
+            vorkomma++;
+            nachkomma -= potenziert;
+        }
+        while (nachkomma < 0) {
+            vorkomma--;
+            nachkomma += potenziert;
+        }
+    }
+
+    public void div(@NotNull MyFloat myFloat) {
+        int myVorkomma = myFloat.vorkomma();
+        int myNachkomma = myFloat.nachkomma();
+        int myPotenziert = myFloat.potenziert();
+
+        if (potenziert < myPotenziert) myNachkomma /= (double) myPotenziert / potenziert;
+        else if (potenziert > myPotenziert) myNachkomma *= (double) potenziert / myPotenziert;
+
+        int tmp = (((vorkomma * potenziert) + nachkomma) * potenziert) /
+                ((myVorkomma * potenziert) + myNachkomma);
+
+        vorkomma = 0;
+        nachkomma = tmp;
+        while (nachkomma >= potenziert) {
+            vorkomma++;
+            nachkomma -= potenziert;
+        }
+        while (nachkomma < 0) {
+            vorkomma--;
+            nachkomma += potenziert;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return vorkomma +
+                "." +
+                nachkomma;
+    }
 }
