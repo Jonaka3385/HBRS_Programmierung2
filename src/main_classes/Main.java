fcba7bd1f243270c62a3d1d732556dbdfeb48ad6
Jonaka <jonakactfs@gmail.com>
2023-05-18 15:05:57 +0200

    MyFloat JUnit fertig

--- src/main_classes/Main.java
+++ src/main_classes/Main.java
@@ -1,21 +1,7 @@
 package main_classes;
 
-import speicher.MyFloat;
-
 public class Main {
     public static void main(String[] args) {
-        MyFloat m1 = new MyFloat(1, 50);
-        MyFloat m2 = new MyFloat(1, 50);
-        m1.add(m2);
-        System.out.println(m1);
-        m1 = new MyFloat(1, 50);
-        m1.sub(m2);
-        System.out.println(m1);
-        m1 = new MyFloat(1, 50);
-        m1.mul(m2);
-        System.out.println(m1);
-        m1 = new MyFloat(1, 50);
-        m1.div(m2);
-        System.out.println(m1);
+
     }
 }
